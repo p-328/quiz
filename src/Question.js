62ceb3713266bbaa4cafdba17fc8979deabf09e2
p-328 <69773020+p-328@users.noreply.github.com>
2022-02-13 18:52:44 -0500

    e

--- src/Question.js
+++ src/Question.js
@@ -6,12 +6,12 @@ export default function Question({ question, answer }) {
         setUserAnswer(input.target.value);
     };
     return (
-        <div style={{borderStyle: 'dashed', width: 450, borderColor: 'blue', borderRadius: 20, borderWidth: 2}}>
+        <div style={{borderStyle: 'dashed', width: 450, borderColor: 'blue', borderRadius: 20, borderWidth: 0.5}}>
             <p style={{fontFamily: 'Segoe UI'}}>{question}</p>
             <input type="text" value={userAnswer} style={{ paddingTop: 3.5 }} onChange={handleChange}></input>
             {
-                userAnswer !== answer ? <p style={{ fontFamily: 'Segoe UI', color: 'red' }}>Incorrect</p> :
-                <p style={{ fontFamily: 'Segoe UI', color: 'green' }}>Correct!</p>
+                userAnswer !== answer ? <p style={{ fontFamily: 'Segoe UI', color: 'red', fontWeight: 'bold' }}>Incorrect</p> :
+                <p style={{ fontFamily: 'Segoe UI', color: 'green', fontWeight: 'bold' }}>Correct!</p>
             }
         </div>
     );
