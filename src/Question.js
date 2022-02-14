c434ec06fb5514d36648a759ad0534123ddb3ae1
p-328 <69773020+p-328@users.noreply.github.com>
2022-02-13 19:47:47 -0500

    e

--- src/Question.js
+++ src/Question.js
@@ -6,7 +6,7 @@ export default function Question({ question, answer }) {
         setUserAnswer(input.target.value);
     };
     return (
-        <div style={{borderStyle: 'dashed', width: 450, borderColor: 'blue', borderRadius: 25, borderWidth: 0.3}}>
+        <div style={{borderStyle: 'dashed', width: 450, borderColor: 'blue', borderRadius: 20, borderWidth: 0.3}}>
             <p style={{fontFamily: 'Segoe UI'}}>{question}</p>
             <input type="text" value={userAnswer} style={{ paddingTop: 3.5 }} onChange={handleChange}></input>
             {
